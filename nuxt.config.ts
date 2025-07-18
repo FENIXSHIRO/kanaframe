d79ce7e89eb61345ee5375ded2462dd624b665ce
FENIXSHIRO <112804060+FENIXSHIRO@users.noreply.github.com>
2025-07-18 12:44:04 +0300

    chore: tailwind css, eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   devServer: {
     port: 3001
-  }
+  },
+  ssr: false,
+  vite: {    plugins: [      tailwindcss(),    ],  },
+  css: ['~/assets/css/main.css'],
 })
